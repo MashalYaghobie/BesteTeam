4f7514cbea8392196e6dd9e0efd7654150f131a7
MashalYaghobie <mashal_yaghobie@outlook.com>
2024-01-11 19:34:44 +0100

    readability

--- rush_hour.py
+++ rush_hour.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class Vehicle:
     def __init__(self, name, length, orientation, start_row, start_col):
         self.name = name # We'll use name to represent vehicles (A, B, C, etc.)
@@ -8,13 +9,14 @@ class Vehicle:
         self.row = start_row
         self.col = start_col
 
+        
 class RushHour:
     def __init__(self):
         # Initializes board and dictionary where vehicles will be stored
         self.board = [['.' for _ in range(6)] for _ in range(6)]
         self.vehicles = {}
         
-    # Code added
+
     def read_all_vehicles(self, file):
         # Reads the csv file
         rush_hour_file = pd.read_csv(file)
@@ -49,12 +51,14 @@ class RushHour:
             for i in range(vehicle.length):
                 self.board[vehicle.row + i][vehicle.col] = vehicle.name
 
+                
     def display_board(self):
         # Displays the board
         for row in self.board:
             print(' '.join(row))
         print()
 
+        
     def is_move_valid(self, vehicle, new_row, new_col):
         """Check if the vehicle can move to the new position without collision
         """
@@ -72,30 +76,31 @@ class RushHour:
                         return False
         return True
 
+    
     def move_vehicle(self, vehicle_id, distance):
         vehicle = self.vehicles[vehicle_id]
         new_row, new_col = vehicle.row, vehicle.col
 
         if vehicle.orientation == 'H':
-            # calculate new column position
+            # Calculate new column position
             new_col += distance
 
-            # check if the move is valid
+            # Check if the move is valid
             if 0 <= new_col <= 5 - vehicle.length + 1 and self.is_move_valid(vehicle, new_row, new_col):
 
-                # clear vehicle's current position
+                # Clear vehicle's current position
                 for i in range(vehicle.length):
                     self.board[vehicle.row][vehicle.col + i] = '.'
-                # update vehicle position
+                # Update vehicle position
                 vehicle.col = new_col
-                # place vehicle at new position
+                # Place vehicle at new position
                 for i in range(vehicle.length):
                     self.board[vehicle.row][vehicle.col + i] = vehicle.name
             else:
                 print("Invalid move")
 
         else:
-            # similar logic for vertical movement
+            # Similar logic for vertical movement
             new_row += distance
             if 0 <= new_row <= 5 - vehicle.length and self.is_move_valid(vehicle, new_row, new_col):
 
@@ -111,12 +116,13 @@ class RushHour:
 
 
     def check_win(self):
-        # check if red car 'R' is in the winning position
+        # Check if red car 'R' is in the winning position
         for row in self.board:
-            if row[-1] == 'R': # checking the rightmost column for the red car
+            if row[-1] == 'R': # Checking the rightmost column for the red car
                 return True
         return False
 
+    
     def play_game(self):
         while True:
             self.display_board()
@@ -134,6 +140,7 @@ class RushHour:
             else:
                 print("Invalid vehicle name. Please try again.")
 
+                
     def get_state(self):
         """Method for getting a string representation of the current
         state of the board.
