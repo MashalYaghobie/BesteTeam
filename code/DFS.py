435db99b5d2d7bc026cc222f6187ed11adb629e0
Bram van de Weert <bvandeweert22@gmail.com>
2024-01-25 13:57:54 +0100

    Eerste logica toegevoegd voor dfs

--- code/DFS.py
+++ code/DFS.py
@@ -1,4 +1,23 @@
 import copy
+from rush_hour import RushHour
+
+def depth_first_search(starting_state, depth):
+    # add starting state to stack
+    stack = [starting_state]
+
+    while len(stack) > 0:
+
+        # get the latest state (top) from the stack
+        latest_state = stack.pop()
+        print(latest_state)
+
+        # stop condition for if the game has been finished
+        if game.check_win(latest_state):
+            print(f"Game has been finished")
+            break
+        
+        
+
 
 # no deeper than depth
 depth = 3
