39b850a7fb8db2ef29e0e5ed2955a909136b9771
Bram van de Weert <bvandeweert22@gmail.com>
2024-01-25 13:29:45 +0100

    Begin code van lecture overgenomen voor DFS

--- code/DFS.py
+++ code/DFS.py
@@ -1,1 +1,28 @@
+import copy
 
+# no deeper than depth
+depth = 3
+
+# add begin state to stack
+stack = [""]
+
+while len(stack)>0:
+
+    # get top from stack
+    stack = stack.pop()
+    print(state)
+
+    # stop condition
+    if len(state) < depth:
+
+        # for each possible action:
+        for i in ['R', 'L']:
+
+            # deepcopy the state
+            child = copy.deepcopy(state)
+
+            # make new child
+            child += i
+
+            # put on stack
+            stack.append(child)
